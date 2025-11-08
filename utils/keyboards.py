5bd4c1bf686bc8cd4becc937eb11fd46fc7ddb34
AsqarUaHa <94336900+AsqarUaHa@users.noreply.github.com>
2025-11-08 10:29:05 +0500

    Update keyboards.py

--- utils/keyboards.py
+++ utils/keyboards.py
@@ -1,1 +1,8 @@
+from telegram import ReplyKeyboardMarkup
+
+def main_menu():
+    return ReplyKeyboardMarkup([
+        ["📍 Check-in", "📘 База знаний"],
+        ["🎯 Конкурсы", "⚙️ Профиль"]
+    ], resize_keyboard=True)
 
