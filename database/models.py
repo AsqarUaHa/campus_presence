bec9f5fafdcb6808f909bbb586c4f6a854d9e79e
AsqarUaHa <94336900+AsqarUaHa@users.noreply.github.com>
2025-11-08 10:27:19 +0500

    Update models.py

--- database/models.py
+++ database/models.py
@@ -1,1 +1,12 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+@dataclass
+class User:
+    telegram_id: int
+    first_name: str
+    last_name: str
+    birth_date: datetime
+    role: str
+    phone: str
 
