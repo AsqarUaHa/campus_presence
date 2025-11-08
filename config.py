29433837e946c4cfc2c167b4bc6a8120b5fa45fc
AsqarUaHa <94336900+AsqarUaHa@users.noreply.github.com>
2025-11-08 08:17:14 +0500

    Update config.py

--- config.py
+++ config.py
@@ -8,3 +8,24 @@ PROXIMITY_RADIUS = int(os.getenv("PROXIMITY_RADIUS", 500))
 TIMEZONE_OFFSET = int(os.getenv("TIMEZONE_OFFSET", 5))
 ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "").split(",") if x]
 
+from enum import Enum, auto
+
+class States(Enum):
+    REGISTRATION_FIRST_NAME = auto()
+    REGISTRATION_LAST_NAME = auto()
+    REGISTRATION_BIRTH_DATE = auto()
+    REGISTRATION_TEAM_ROLE = auto()
+    REGISTRATION_PHONE = auto()
+
+    ADMIN_POST_TEXT = auto()
+    ADMIN_POST_MEDIA = auto()
+    ADMIN_POST_DATETIME = auto()
+
+    ADMIN_EVENT_NAME = auto()
+    ADMIN_EVENT_START = auto()
+    ADMIN_EVENT_END = auto()
+    ADMIN_EVENT_DESC = auto()
+
+    ADMIN_KB_TITLE = auto()
+    ADMIN_KB_FILE = auto()
+
