2708094b53532e0b92ad78d59cdef1c1d9e89647
AsqarUaHa <94336900+AsqarUaHa@users.noreply.github.com>
2025-11-08 10:29:20 +0500

    Update decorators.py

--- utils/decorators.py
+++ utils/decorators.py
@@ -1,1 +1,13 @@
+from functools import wraps
+from config import ADMIN_IDS
+
+def admin_only(func):
+    @wraps(func)
+    async def wrapper(update, context, *args, **kwargs):
+        user_id = update.effective_user.id
+        if user_id not in ADMIN_IDS:
+            await update.message.reply_text("⛔ Доступ только для админов.")
+            return
+        return await func(update, context, *args, **kwargs)
+    return wrapper
 
