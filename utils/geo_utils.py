b7fce47e5c1c25bef50596ed9519848e339064fe
AsqarUaHa <94336900+AsqarUaHa@users.noreply.github.com>
2025-11-08 10:28:50 +0500

    Update geo_utils.py

--- utils/geo_utils.py
+++ utils/geo_utils.py
@@ -1,1 +1,5 @@
+from geopy.distance import distance
+
+def is_within_radius(user_lat, user_lon, target_lat, target_lon, radius_m):
+    return distance((user_lat, user_lon), (target_lat, target_lon)).meters <= radius_m
 
