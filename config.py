0048770dedcc60cbcd4e7b5f6e704a297b0e8cfa
AsqarUaHa <94336900+AsqarUaHa@users.noreply.github.com>
2025-11-08 08:10:07 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,10 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+DATABASE_URL = os.getenv("DATABASE_URL")
+CAMPUS_LATITUDE = float(os.getenv("CAMPUS_LATITUDE", 43.239581))
+CAMPUS_LONGITUDE = float(os.getenv("CAMPUS_LONGITUDE", 76.962465))
+PROXIMITY_RADIUS = int(os.getenv("PROXIMITY_RADIUS", 500))
+TIMEZONE_OFFSET = int(os.getenv("TIMEZONE_OFFSET", 5))
+ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "").split(",") if x]
 
